8a982f7f88abfe2be015a9b61288b6e0de6a155b
Made By IToncek <toncek2019@post.cz>
2023-09-30 20:10:55 +0200

    WIP

--- ChunkLockPrototype/src/main/java/space/itoncek/eventmanager/chunklockprototype/ChunkLockPrototype.java
+++ ChunkLockPrototype/src/main/java/space/itoncek/eventmanager/chunklockprototype/ChunkLockPrototype.java
@@ -1,19 +1,34 @@
 package space.itoncek.eventmanager.chunklockprototype;
 
+import org.bukkit.Chunk;
 import org.bukkit.plugin.java.JavaPlugin;
+import space.itoncek.eventmanager.chunklockprototype.commands.SetupCommand;
+import space.itoncek.eventmanager.chunklockprototype.listeners.BlockClickListener;
+import space.itoncek.eventmanager.chunklockprototype.listeners.BlockStuffListener;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ChunkLockPrototype extends JavaPlugin {
 
     public static ChunkLockPrototype pl;
-
+    public static List<Chunk> unlockedChunks = new ArrayList<>();
     @Override
     public void onEnable() {
         // Plugin startup logic
+        File cache = new File("./chunklock-cache/");
+        if (cache.exists()) cache.delete();
         pl = this;
+        getCommand("setupcl").setExecutor(new SetupCommand());
+        getServer().getPluginManager().registerEvents(new BlockStuffListener(), this);
+        getServer().getPluginManager().registerEvents(new BlockClickListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        File cache = new File("./chunklock-cache/");
+        if (cache.exists()) cache.delete();
     }
 }
--- ChunkLockPrototype/src/main/java/space/itoncek/eventmanager/chunklockprototype/commands/SetupCommand.java
+++ ChunkLockPrototype/src/main/java/space/itoncek/eventmanager/chunklockprototype/commands/SetupCommand.java
@@ -1,59 +1,84 @@
 package space.itoncek.eventmanager.chunklockprototype.commands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Chunk;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Random;
 
 import static space.itoncek.eventmanager.chunklockprototype.ChunkLockPrototype.pl;
 
 public class SetupCommand implements CommandExecutor {
-
-    private List<Block> replace = new ArrayList<>();
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (sender.isOp() && args.length == 1) {
             for (int x = -Integer.parseInt(args[0]); x <= Integer.parseInt(args[0]); x++) {
                 for (int z = -Integer.parseInt(args[0]); z < Integer.parseInt(args[0]); z++) {
-                    World w = Objects.requireNonNull(Bukkit.getWorld("world"));
-                    Chunk c = w.getChunkAt(x, z);
-
-                    for (int by = w.getMinHeight(); by <= w.getMaxHeight(); by++) {
-                        for (int bx = 0; bx < 16; bx++) {
-                            for (int bz = 0; bz < 16; bz++) {
-                                Block block = c.getBlock(bx, by, bz);
-                                if (!block.isEmpty() || !block.isLiquid()) continue;
-                                if (bx == 0 || bx == 15 || bz == 0 || bz == 15) {
-                                    replace.add(block);
+                    int finalX = x;
+                    int finalZ = z;
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            World w = Objects.requireNonNull(Bukkit.getWorld("world"));
+                            Chunk c = w.getChunkAt(finalX, finalZ);
+                            Bukkit.getLogger().info("[CL] Adding chunk %d,%d".formatted(finalX, finalZ));
+                            JSONArray replacements = new JSONArray();
+                            for (int by = w.getMinHeight(); by <= w.getMaxHeight(); by++) {
+                                for (int bx = 0; bx < 16; bx++) {
+                                    for (int bz = 0; bz < 16; bz++) {
+                                        Block block = c.getBlock(bx, by, bz);
+                                        if ((bx == 0 || bx == 15 || bz == 0 || bz == 15)) {
+                                            JSONObject replacement = new JSONObject();
+                                            replacement.put("material", block.getType());
+                                            replacement.put("location", toJsonObject(block.getLocation()));
+                                            replacements.put(replacement);
+                                            block.setType(block.isSolid() ? Material.LIGHT_BLUE_CONCRETE : Material.LIGHT_BLUE_STAINED_GLASS);
+                                        }
+                                    }
                                 }
                             }
+
+                            File output = new File("./chunklock-cache/%d/%d.json".formatted(finalX, finalZ));
+                            if (!output.getParentFile().exists()) output.getParentFile().mkdirs();
+                            if (output.exists()) output.delete();
+                            try (FileWriter fw = new FileWriter(output)) {
+                                fw.write(replacements.toString());
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
                         }
-                    }
+                    }.runTaskLater(pl, Math.round(new Random().nextDouble(0.0, 4.0) * 20));
+
                 }
             }
 
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < 64; i++) {
-                        Block block = replace.get(0);
-                        block.setType(Material.LIGHT_BLUE_STAINED_GLASS);
-                        replace.remove(0);
-                    }
-                }
-            }.runTaskTimer(pl, 20L, 1L);
+
         }
         return true;
+    }
+
+    private JSONObject toJsonObject(Location location) {
+        JSONObject ret = new JSONObject();
+
+        ret.put("world", location.getWorld().getName());
+        ret.put("x", location.x());
+        ret.put("y", location.y());
+        ret.put("z", location.z());
+
+        return ret;
+    }
+
+    record ReplaceAction(Block b, int type) {
     }
 }
