66ea44250fafe659c826bc9e1982fbf1f778d92b
Made By IToncek <toncek2019@post.cz>
2023-11-11 21:56:56 +0100

    DecisionDomeDecider work

--- DecisionDomeDecider/src/main/java/space/itoncek/csyt/decisiondomedecider/DecisionDomeDecider.java
+++ DecisionDomeDecider/src/main/java/space/itoncek/csyt/decisiondomedecider/DecisionDomeDecider.java
@@ -1,6 +1,9 @@
 package space.itoncek.csyt.decisiondomedecider;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import space.itoncek.csyt.DRMLib;
+import space.itoncek.csyt.UpdateLib;
 
 public final class DecisionDomeDecider extends JavaPlugin {
     public static DecisionDomeDecider ddd;
@@ -9,6 +12,13 @@ public final class DecisionDomeDecider extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        new DRMLib() {
+            @Override
+            public void callback() {
+                Bukkit.shutdown();
+            }
+        };
+        UpdateLib.downloadCommitID(this.getDataFolder());
         ddd = this;
         DDDCommand dddc = new DDDCommand();
         getCommand("ddd").setTabCompleter(dddc);
@@ -18,5 +28,6 @@ public final class DecisionDomeDecider extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        UpdateLib.checkForUpdates(this.getDataFolder(), "DecisionDomeDecider", this.getFile());
     }
 }
