827bb35738444686ea61b123a40a1b7460d08ee5
Made By IToncek <toncek2019@post.cz>
2023-11-12 12:23:00 +0100

    CGI Changes

---
+++ CSYT-RTS/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace CSYT_RTS
+{
+    internal class Program : WebSocketBehavior
+    {
+        static WebSocketServer server;
+        static void Main(string[] args)
+        {
+            server = new WebSocketServer(2232);
+            server.AddWebSocketService<Program>("/");
+            server.Start();
+            while (server.IsListening) { }
+        }
+
+        async protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.BroadcastAsync(e.Data, ()=>{});
+        }
+    }
+}
