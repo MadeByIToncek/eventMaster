20248315e7a0ed3882ee02cdf5126e4ea07aec30
Made By IToncek <toncek2019@post.cz>
2023-11-29 20:27:19 +0100

    CommLib code styling + implementing AutoCloseable interface to enable usage in try() blocks

--- commons/src/main/java/space/itoncek/csyt/CommLib.java
+++ commons/src/main/java/space/itoncek/csyt/CommLib.java
@@ -1,20 +1,46 @@
 package space.itoncek.csyt;
+
+import org.jetbrains.annotations.NotNull;
+
 import java.sql.*;
 
-public abstract class CommLib {
-    //public String DB_URL = "mysql://CHANGE_THIS";
-    //public String DB_USER = "CHANGE_THIS";
-    //public String DB_PASS = "CHANGE_THIS";
+public abstract class CommLib implements AutoCloseable {
+    private final Connection conn;
 
-    public boolean GetPlayer(String DB_URL, String DB_USER, String DB_PASS) {
-        try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
-            PreparedStatement stmt = (PreparedStatement) conn.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT *");
-            stmt.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
+    /**
+     * Creates new DB connection
+     *
+     * @param url  URL address of the database instance, cannot be null
+     * @param user MySQL User
+     * @param pass MySQL User's password
+     * @throws SQLException DB access error / url == null
+     */
+    public CommLib(@NotNull String url, String user, String pass) throws SQLException {
+        conn = DriverManager.getConnection(url, user, pass);
+    }
+
+    /**
+     * Request player object from database
+     *
+     * @param username username of player you want to query
+     * @return TODO
+     * @throws SQLException most likely DB access error
+     */
+    public boolean GetPlayer(String username) throws SQLException {
+        PreparedStatement stmt = (PreparedStatement) conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT *");
+        stmt.close();
+
         return true;
+    }
+
+    /**
+     * Closes this connection
+     *
+     * @throws SQLException DB access error
+     */
+    @Override
+    public void close() throws SQLException {
+        conn.close();
     }
 }
