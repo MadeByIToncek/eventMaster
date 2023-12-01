9d6632b4d6696b7619b2f37dd15e1dde4110fab4
Made By IToncek <toncek2019@post.cz>
2023-12-01 21:38:28 +0100

    CommLibTest

--- commons/src/test/java/space/itoncek/csyt/comm/CommLibTest.java
+++ commons/src/test/java/space/itoncek/csyt/comm/CommLibTest.java
@@ -4,12 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CommLibTest {
 
     @Test
-    void getPlayer() throws SQLException{
-        CommLib lib = new CommLib("jdbc:mysql://cloud.itoncek.space:3306/s2_proxytest", "u2_J4yDWKHPiC", "MadJcFVUqz^xZw@1LLxycaR^") {};
+    void getPlayer() throws SQLException {
+        CommLib lib = new CommLib(System.getenv("DB_URL"), System.getenv("DB_USER"), System.getenv("DB_PWD")) {
+        };
+
+        CSYTPlayer player = lib.getPlayer("IToncek");
+        lib.close();
+
+        System.out.println(player.toString());
+        assertEquals(player.name(), "IToncek");
+        assertEquals(player.points(), 0);
+        assertEquals(player.team(), Team.spectator);
     }
 }
